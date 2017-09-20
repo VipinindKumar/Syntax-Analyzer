b936b33beecd67ff4a92d62482c8a645165fb1f0
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-09-20 14:40:17 +0000

    Initial commit with some skeleton defined for the classes

---
+++ JackAnalyzer.py
@@ -0,0 +1,6 @@
+class JackAnalyzer:
+    '''Main class to run the JackAnalyzer
+    by using JackTokenizerand CompilationEngine'''
+    
+    if __name__ == '__main__':
+        
---
+++ JackTokenizer.py
@@ -0,0 +1,11 @@
+class JackTokenizer:
+    ''' class used to tokenize the input
+    file(s)? into a stream of tokens, ignoring
+    whitespace, comments etc.'''
+    
+    def __init__(self, inFile):
+        'constructor for JackTokenizer'
+        
+    
+    def hasMoreTokens():
+        ''
