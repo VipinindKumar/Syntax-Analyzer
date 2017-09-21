7c4bb4e284d369f810eb680ff39bb8e3728face6
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-09-21 04:19:22 +0000

    Create class CompilationEngine with API and Doc for constructor and method compileClass(), compileClassVarDec(), compileSubroutine() and compileParameterList()

---
+++ CompilationEngine.py
@@ -0,0 +1,27 @@
+class CompilationEngine:
+    '''Gets its input from JackTokenizer and 
+    emits it parsed structure in output stream/file'''
+    
+    def __init__(self, inFile, outFile):
+        '''Creates a new CompilationEngine with given
+        input and output. The next routine called must
+        be compileClass()'''
+        
+    
+    def compileClass(self):
+        'Compiles a complete class'
+        
+    
+    def compileClassVarDec(self):
+        'Compiles a static or a field declaration'
+        
+    
+    def compileSubroutine(self):
+        'Compiles a complete method, function or constructor'
+        
+    
+    def CompileParameterList(self):
+        'Compiles a parameter list not including the enclosing ()'
+        
+    
+    
