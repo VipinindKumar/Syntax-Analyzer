eeee9bbeffc4793e23c06c827cc25ab99ae15a55
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-09-24 16:36:01 +0000

    Create main functionin class JackAnalyzer to test the JackTokenizer class

--- JackAnalyzer.py
+++ JackAnalyzer.py
@@ -1,6 +1,34 @@
+import JackTokenizer
+import sys
+
 class JackAnalyzer:
     '''Main class to run the JackAnalyzer
     by using JackTokenizerand CompilationEngine'''
     
     if __name__ == '__main__':
-        
+        tokenizer = JackTokenizer(sys.argv[0])
+        with open(sys.argv[0][0:-4] + 'xml', 'w') as outfile:
+            outfile.write('<Token>\n\n    ')
+            
+            while tokenizer.hasMoreTokens():
+                tokenizer.advance()
+                
+                ttype = tokenizer.tokenType()
+                
+                if ttype == 'KEYWORD':
+                    outfile.write('<keyword>' + tokenizer.keyword() + '<\keyword>\n    ')
+                
+                elif ttype == 'SYMBOL':
+                    outfile.write('<symbol>' + tokenizer.symbol() + '<\symbol>\n    ')
+                
+                elif ttype == 'IDENTIFIER':
+                    outfile.write('<identifier>' + tokenizer.identifier() + '<\identifier>\n    ')
+                
+                elif ttype == 'INT_CONST':
+                    outfile.write('<intVal>' + tokenizer.intVal() + '<\intval>\n    ')
+                
+                elif ttype == 'STRING_CONST':
+                    outfile.write('<stringVal>' + tokenizer.stringVal() + '<\stringVal>\n    ')
+                
+            outfile.write('\n<\Token>')
+        outfile.close()
