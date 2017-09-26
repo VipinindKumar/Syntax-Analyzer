aed2e043b115c9d1703c4f6f32b0ff8a596a0641
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-09-26 06:35:22 +0000

    Change the implementation to convert some symbols to their character reference in xml, by fixing some bugs

--- JackAnalyzer.py
+++ JackAnalyzer.py
@@ -1,38 +1,53 @@
 import JackTokenizer
 import sys
 
-class JackAnalyzer:
-    '''Main class to run the JackAnalyzer
-    by using JackTokenizerand CompilationEngine'''
+'''Main class to run the JackAnalyzer
+by using JackTokenizer and CompilationEngine'''
+
+
+#Change <, >, &, " to their respective character reference - &lt;, &gt;, &amp;, &quot;
+def charRef(sym):
+    if sym == '<':
+        return '&lt;'
+    elif sym == '>':
+        return '&gt;'
+    elif sym == '&':
+        return '&amp;'
+    elif sym == '"':
+        return '&quot;'
+    else:
+        return sym
     
-    if __name__ == '__main__':
-        tokenizer = JackTokenizer.JackTokenizer(sys.argv[0])
-        with open(sys.argv[0][0:-4] + 'xml', 'w') as outfile:
-            outfile.write('<Token>\n')
+    
+if __name__ == '__main__':
+    tokenizer = JackTokenizer.JackTokenizer(sys.argv[0])
+    with open(sys.argv[0][0:-4] + 'xml', 'w') as outfile:
+        outfile.write('<Token>\n')
+        
+        while tokenizer.hasMoreTokens():
+            tokenizer.advance()
             
-            while tokenizer.hasMoreTokens():
-                tokenizer.advance()
+            ttype = tokenizer.tokenType()
+            
+            if ttype == 'KEYWORD':
+                outfile.write('    <keyword> ' + tokenizer.keyword() + ' <\keyword>\n')
+            
+            elif ttype == 'SYMBOL':
+                sym = charRef(tokenizer.symbol())
                 
-                ttype = tokenizer.tokenType()
-                
-                if ttype == 'KEYWORD':
-                    outfile.write('    <keyword> ' + tokenizer.keyword() + ' <\keyword>\n')
-                
-                elif ttype == 'SYMBOL':
-                    outfile.write('    <symbol> ' + tokenizer.symbol() + ' <\symbol>\n')
-                
-                elif ttype == 'IDENTIFIER':
-                    outfile.write('    <identifier> ' + tokenizer.identifier() + ' <\identifier>\n')
-                
-                elif ttype == 'INT_CONST':
-                    outfile.write('    <intConstant> ' + tokenizer.intVal() + ' <\intConstant>\n')
-                
-                elif ttype == 'STRING_CONST':
-                    outfile.write('    <stringConstant> ' + tokenizer.stringVal() + ' <\stringConstant>\n')
-                
-            outfile.write('<\Token>')
-        outfile.close()
+                outfile.write('    <symbol> ' + sym + ' <\symbol>\n')
+            
+            elif ttype == 'IDENTIFIER':
+                outfile.write('    <identifier> ' + tokenizer.identifier() + ' <\identifier>\n')
+            
+            elif ttype == 'INT_CONST':
+                outfile.write('    <intConstant> ' + tokenizer.intVal() + ' <\intConstant>\n')
+            
+            elif ttype == 'STRING_CONST':
+                outfile.write('    <stringConstant> ' + tokenizer.stringVal() + ' <\stringConstant>\n')
+            
+        outfile.write('<\Token>')
+    outfile.close()
         
 
-#!!! Change <, >, &, " to their respective character reference - &lt;, &gt;, &amp;, &quot;
 #!!! Handle comments in jack file
