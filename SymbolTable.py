dcc0101acff801811923b6a6f9f37f140a7f2ee5
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-10-07 12:30:37 +0000

    Create SymbolTable class with its documentation

---
+++ SymbolTable.py
@@ -0,0 +1,8 @@
+class SymbolTabel:
+    """ Symbol Table is a class that stores an identifier 
+        and all its properties like name, type, kind and runnign index
+        in a python dictionary. For two scopes of Jack program
+        one for class level and other for subroutine level.
+        It also provides different methods to retrieve them """
+    
+    
