c5a58f29ce1a76909258d407db542a4fff32454b
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-10-07 12:49:47 +0000

    Add API for the constructor, startsubroutine and define method in the class with their documentation

--- SymbolTable.py
+++ SymbolTable.py
@@ -5,4 +5,22 @@ class SymbolTabel:
         one for class level and other for subroutine level.
         It also provides different methods to retrieve them """
     
+    def __init__(self):
+        """ Creates a new empty symbol table """
+        
+        
+    
+    def startSubroutine(self):
+        """ Starts a new subroutine scope, ie.
+            resets the subroutine's symbole table """
+        
+        
+    
+    def define(self, name, vartype, kind):
+        """ Defines a new identifier of a given name, vartype and kind and
+            assigns it a running index.
+            STATIC and FIELD identifiers have a class scope, while 
+            VAR and ARG identifiers have a subroutine scope """
+        
+        
     
