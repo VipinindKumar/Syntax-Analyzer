c9de387cb9a80f2c2ad973f1d7322e8dad5c0bf9
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-10-08 03:52:15 +0000

    Implement define() method and added some instance variables in the constructor and startSubroutine() method

--- SymbolTable.py
+++ SymbolTable.py
@@ -9,12 +9,18 @@ class SymbolTabel:
         """ Creates a new empty symbol table """
         
         self.classDict = {}
+        
+        self.static = 0
+        self.field = 0
     
     def startSubroutine(self):
         """ Starts a new subroutine scope, ie.
             resets the subroutine's symbole table """
         
         self.subDict = {}
+        
+        self.arg = 0
+        self.var = 0
     
     def define(self, name, vartype, kind):
         """ Defines a new identifier of a given name, vartype and kind and
@@ -22,7 +28,20 @@ class SymbolTabel:
             STATIC and FIELD identifiers have a class scope, while 
             VAR and ARG identifiers have a subroutine scope """
         
-        
+        if kind == 'STATIC':
+            self.classDict[name] = [vartype, kind, self.static]
+            self.static += 1
+        elif kind == 'FIELD':
+            self.classDict[name] = [vartype, kind, self.field]
+            self.field += 1
+        elif kind == 'ARG':
+            self.subDict[name] = [vartype, kind, self.arg]
+            self.arg += 1
+        elif kind == 'VAR':
+            self.subDict[name] = [vartype, kind, self.var]
+            self.var += 1
+        else:
+            raise ValueError('Not a valid kind of identifier')
     
     def varCount(self, kind):
         """ Returns the number of variables of the given kind already defined
