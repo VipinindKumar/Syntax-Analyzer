a8fc7a19a4438abbb19ccec2e7d1658a84279f77
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-10-10 07:45:07 +0000

    Create the class VMWriter  and add API and Documentaiton for constructor, writePush() and writePop()

---
+++ VMWriter.py
@@ -0,0 +1,21 @@
+class VMWriter:
+    """ Emits VM commands into a file, using the VM command syntax """
+    
+    def __init__(self, outfile):
+        """ Creates a new file and prepare it for writing """
+        
+        
+    
+    def writePush(self, segment, index):
+        """ Writes a VM push command
+            segment: CONST, ARG, LOCAL, STATIC, THIS, THAT, POINTER, TEMP """
+        
+        
+    
+    def writePop(self, segment, index):
+        """ Writes a VM pop command
+            segment: CONST, ARG, LOCAL, STATIC, THIS, THAT, POINTER, TEMP """
+        
+        
+    
+    def 
