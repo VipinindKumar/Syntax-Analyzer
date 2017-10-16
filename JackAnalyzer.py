2fe04ceeada666dfaf155bdbdcc055bc98c5e5eb
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-10-16 06:25:57 +0000

    fix the problem of not complete path to the file to be read

--- JackAnalyzer.py
+++ JackAnalyzer.py
@@ -12,8 +12,8 @@ if __name__ == '__main__':
         compiler.compileClass()
     # else if it's a directory, then cycle through all the files ending in .jack to compile all classes
     else:
-        for folder, subFolders, files in os.walk(sys.argv[1]):
+        for fol, subFol, files in os.walk(sys.argv[1]):
             for file in files:
                 if file.endswith('.jack'):
-                    compiler = CompilationEngine.CompilationEngine(file, file[:-4] + 'xml')
+                    compiler = CompilationEngine.CompilationEngine(os.path.join(fol, file), os.path.join(fol, file[:-4] + 'xml'))
                     compiler.compileClass()
